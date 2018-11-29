eb6dd1ed3a0df628919138b3eb5d12b85446ca76
XinAnzzZ <13023195022@163.com>
2018-11-29 17:47:16 +0800

    shell基础

--- assets/js/clickword.js
+++ assets/js/clickword.js
@@ -1,33 +1,34 @@
-onload = function() {
-  clickWord();
+onload = function () {
+    clickWord();
 };
 
 function clickWord() {
-  var a_idx = 0;
-  jQuery(document).ready(function($) {
-    $("body").click(function(e) {
-      var a = new Array("我老婆", "Yoda", "天下", "第一美", "！！！");
-      var $i = $("<span/>").text(a[a_idx]);
-      a_idx = (a_idx + 1) % a.length;
-      var x = e.pageX,
-        y = e.pageY;
-      $i.css({
-        "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
-        "top": y - 20,
-        "left": x,
-        "position": "absolute",
-        "font-weight": "bold",
-        "color": "#ff6651"
-      });
-      $("body").append($i);
-      $i.animate({
-          "top": y - 180,
-          "opacity": 0
-        },
-        1500,
-        function() {
-          $i.remove();
+    let a_idx = 0;
+    jQuery(document).ready(function ($) {
+        $("body").click(function (e) {
+            let a = ["我老婆", "Yoda", "天下", "第一美", "！！！"];
+            let $i = $("<span/>").text(a[a_idx]);
+            a_idx = (a_idx + 1) % a.length;
+            let x = e.pageX,
+                y = e.pageY;
+            $i.css({
+                "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
+                "top": y - 20,
+                "left": x,
+                "position": "absolute",
+                "font-weight": "bold",
+                "font-size": "20px",
+                "color": "#ff6651"
+            });
+            $("body").append($i);
+            $i.animate({
+                    "top": y - 180,
+                    "opacity": 0
+                },
+                1500,
+                function () {
+                    $i.remove();
+                });
         });
     });
-  });
 }
